c97ace3448ab7d8e069b8bdfc94b9149eaffb987
mariana <mariana.a2006@aluno.ifsc.edu.br>
2025-03-24 09:12:48 -0300

    imc com uma atividade

--- app/src/main/java/com/ifsc/contaclicks/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclicks/MainActivity.java
@@ -3,38 +3,43 @@ package com.ifsc.contaclicks;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+//Nao reconheceu a classe = ultimo carater + ctrol + tecla do espaço
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 int i=0;
+EditText edpeso,edaltura;
+
+TextView tvresultadoimc;
+
+Button buttonCalcular;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         setContentView(R.layout.activity_main);
-        //Associando objeto interface a variavel local
-         TextView tv = findViewById(R.id.textView);
-          tv.setText(getString(R.string.app_name));
+        setContentView(R.layout.activity_main);
+        edpeso = findViewById(R.id.edpeso);
+        edaltura = findViewById(R.id.edaltura);
+        tvresultadoimc = findViewById(R.id.tvresultadoimc);
+        buttonCalcular = findViewById(R.id.button);
+        //define um tratamento para o click no botao
 
-          Button b= findViewById(R.id.button);
-          b.setOnClickListener(v -> {//Seu codigo aqui
-              });
+        buttonCalcular.setOnClickListener(v ->{
+            //calcular o IMC
+            double peso,altura,IMC;
 
-          b.setOnClickListener(new View.OnClickListener(){
-              public void onClick(View v){
-            tv.setText(Integer.toString(i));
-            i++;
-              }
-
-
-          } );
+            peso = Double.parseDouble(edpeso.getText().toString());
+            altura = Double.parseDouble(edaltura.getText().toString());
 
 
-        }
+           IMC = peso /(altura*altura);
+          tvresultadoimc.setText(Double.toString(IMC));
+
+
+        });
+    }
 
 }
