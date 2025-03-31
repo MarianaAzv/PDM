2f584bdba644319a684e9e35ffd6173614695158
mariana <mariana.a2006@aluno.ifsc.edu.br>
2025-03-31 08:28:05 -0300

    Metodos do ciclo de vida

--- app/src/main/java/com/ifsc/contaclicks/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclicks/MainActivity.java
@@ -1,12 +1,15 @@
 package com.ifsc.contaclicks;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 //Nao reconheceu a classe = ultimo carater + ctrol + tecla do espaço
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.DecimalFormat;
@@ -22,6 +25,8 @@ Button buttonCalcular;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("Ciclo de vida","metodo onCreate");
+
         setContentView(R.layout.activity_main);
         edpeso = findViewById(R.id.edpeso);
         edaltura = findViewById(R.id.edaltura);
@@ -46,6 +51,43 @@ Button buttonCalcular;
 
 
         });
+
+
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("Ciclo de vida","metodo onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Ciclo de vida","metodo onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Ciclo de vida","metodo onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Ciclo de vida","metodo onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Ciclo de vida","metodo onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Ciclo de vida","metodo onDestroy");
+    }
 }
