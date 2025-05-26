ed6df894b0ec19b40bfe6db608cd5f31b5ef5cbf
mariana <mariana.a2006@aluno.ifsc.edu.br>
2025-05-26 09:21:51 -0300

    Com listagem

--- app/src/main/java/com/ifsc/contaclicks/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclicks/MainActivity.java
@@ -1,8 +1,14 @@
 package com.ifsc.contaclicks;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,30 +17,51 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 int i=0;
+
+SQLiteDatabase db;
+Button butttonInsere;
+EditText editText;
+ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_main);
-        //Associando objeto interface a variavel local
-         TextView tv = findViewById(R.id.textView);
-          tv.setText(getString(R.string.app_name));
+         db= openOrCreateDatabase("banco",MODE_PRIVATE, null);
 
-          Button b= findViewById(R.id.button);
-          b.setOnClickListener(v -> {//Seu codigo aqui
-              });
+         db.execSQL("CREATE TABLE IF NOT EXISTS notas(id INTEGER  PRIMARY KEY AUTOINCREMENT, txt VARCHAR)");
+         butttonInsere = findViewById(R.id.button);
+        editText=findViewById(R.id.textView);
+        listView=findViewById(R.id.listView);
+         butttonInsere.setOnClickListener(v ->{
+             String msg = editText.getText().toString();
+             insereNota(msg);
+                 });
 
-          b.setOnClickListener(new View.OnClickListener(){
-              public void onClick(View v){
-            tv.setText(Integer.toString(i));
-            i++;
-              }
-
-
-          } );
-
-
+listagemNotas();
+//Falta chamar quando atualiza
         }
 
+        public void listagemNotas(){
+            Cursor cursor= db.rawQuery("SELECT * FROM notas",null);
+            cursor.moveToFirst();
+            ArrayList<String> listaNotas =  new ArrayList<String>();
+
+            while(!cursor.isAfterLast()){
+                int coluna = cursor.getColumnIndex("txt");
+                listaNotas.add(cursor.getString(coluna));
+                cursor.moveToNext();
+            }
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, android.R.id.text1,listaNotas);
+            listView.setAdapter(adapter);
+        }
+public  void insereNota(String txt){
+        // db.execSQL("INSERT INTO notas(txt) VALUES('"+txt+')");
+    ContentValues cv= new ContentValues();
+    cv.put("txt",txt);
+    db.insert("notas",null,cv);
+}
 }
