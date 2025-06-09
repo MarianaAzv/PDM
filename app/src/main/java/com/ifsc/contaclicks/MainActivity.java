ce6f07108c69624b40dfb4964b493faddceef1dd
João Pedro Minella <joao.m2007@aluno.ifsc.edu.br>
2025-06-09 08:45:05 -0300

    Sensores

--- app/src/main/java/com/ifsc/contaclicks/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclicks/MainActivity.java
@@ -1,42 +1,43 @@
 package com.ifsc.contaclicks;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
     Integer i = 0;
+
+    SensorManager mSensorManager;
+    Sensor sensor;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Associando objeto informando a variavel local
-        TextView tv = findViewById(R.id.TextView);
-        tv.setText(getString(R.string.app_name));
+        tv = findViewById(R.id.TextView);
 
-        Button b = findViewById(R.id.button);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
 
-        b.setOnClickListener(new View.OnClickListener() {
+        mSensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
-            @Override
-            public void onClick(View v) {
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        tv.setText(Float.toString(sensorEvent.values[0]));
+    }
 
-                tv.setText(Integer.toString(i));
-                i++;
-
-            }
-        });
-
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 }
